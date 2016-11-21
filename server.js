598f62a9c4f8ebd94427d70102f5ae54a31f09d0
Kevin Giffone <kevin.giffone@gmail.com>
2016-11-21 20:56:44 +0000

    solution

--- server.js
+++ server.js
@@ -2,10 +2,42 @@ var express = require("express");
 
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/:time', function (req, res) 
+{
+  try 
+  {
+    var timeInput = parseInt(req.params.time);
+    var date;
+    if(timeInput)
+    {
+      date = new Date(timeInput);
+    }
+    else
+    {
+      date = new Date(req.params.time);
+    }
+    var unixTime = date.getTime();
+    var time = date.toDateString();
+  
+    var timeObject = 
+    {
+      "unix": unixTime, 
+      "natural": time
+    };
+    res.send(JSON.stringify(timeObject));
+  } 
+  catch (e) 
+  {
+    var timeObject = 
+    {
+      "unix": null, 
+      "natural": null
+    };
+    res.send(JSON.stringify(timeObject));
+  }
+});
 
-app.listen(8080, function () {
+app.listen(8080, function () 
+{
   console.log('Example app listening on port 8080!')
-})
+});
